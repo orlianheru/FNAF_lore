f766fae890048d2d22e1ba7f8a7ef30d1963cd39
JauzaNaufal <541231095@student.smktelkom-pwt.sch.id>
2024-09-17 14:12:41 +0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,46 @@
-document.getElementById('LoginForm').addEventListener('submit', function(event){
-    event.preventDefault();
-    console.log('Form Submitted');
+document.addEventListener('DOMContentLoaded', function() {
+    const blinkTexts = document.querySelectorAll('.blink');
+    setInterval(() => {
+        blinkTexts.forEach(blinkText => {
+            blinkText.style.visibility = blinkText.style.visibility === 'hidden' ? 'visible' : 'hidden';
+        });
+    }, 500); 
+});
 
-    const correctUsername = 'peler';
-    const correctPassword = '12345';
+document.addEventListener('DOMContentLoaded', function() {
+    const images = [
+        {
+            id: 'FreddyImages',
+            defaultSrc: '/assets/images/Freddy.png',
+            hoverSrc: '/assets/images/freddy_creppy.png'
+        },
+        {
+            id: 'BonnieImages',
+            defaultSrc: '/assets/images/Bonnie.png',
+            hoverSrc: '/assets/images/bonnie_creppy.png'
+        },
+        {
+            id: 'ChicaImages',
+            defaultSrc: '/assets/images/cika.png',
+            hoverSrc: '/assets/images/Chica_creppy.png'
+        },
+        {
+            id: 'FoxyImages',
+            defaultSrc: '/assets/images/Foxy.png',
+            hoverSrc: '/assets/images/foxy_creppy.png'
+        }
+    ];
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    images.forEach(image => {
+        const imgElement = document.getElementById(image.id);
+        if (imgElement) {
+            imgElement.addEventListener('mouseover', function() {
+                imgElement.src = image.hoverSrc;
+            });
 
-    if(username === correctUsername && password === correctPassword){
-        alert('Login Berhasil');
-        window.location.href = 'index.html';
-    }else{
-        alert('Login Gagal');
-    }
+            imgElement.addEventListener('mouseout', function() {
+                imgElement.src = image.defaultSrc;
+            });
+        }
+    });
 });
